5acada795088892231f48a099e8f6374afb39637
VincentRanger <dakshayoffice@gmail.com>
2025-02-17 17:21:02 +0530

    App feedback done

--- src/components/Aap.jsx
+++ src/components/Aap.jsx
@@ -30,11 +30,14 @@ import Piechart from "./charts/Piechart";
 import Stackedchart from "./charts/Stackedchart";
 import Loader from "react-js-loader";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-const Aap = () => {
+const Aap = ({open, setOpen}) => {
   const userName= localStorage.getItem("userName")
   const userRole= localStorage.getItem("userRole");
   const [loading,setLoading]=useState(true)
+
+const location=useLocation()
 
 useEffect(()=>{
   const timer= setTimeout(()=>{
@@ -45,7 +48,7 @@ setLoading(false)
 if(loading){
   return(
     <div className="justify-center items-center mt-60">
-      <Loader type='spinner-default' size={100} bgColor="gray"/>
+      <Loader type='spinner-default' size={100} bgColor="green"/>
     </div>
   )
 }
@@ -74,10 +77,10 @@ if(loading){
           className="bg-cover col-span-1 rounded-2xl px-5 py-4"
           style={{ backgroundImage: `url(${back2})` }}
         >
-          <h1 className="text-green-400 text-sm font-bold mt-40">
+          <h1 className="text-green-400 text-sm font-bold mt-10">
             FEATURED APP
           </h1>
-          <h1 className="text-white text-lg font-bold mb-2 mt-2">
+          <h1 className="text-white text-lg font-bold mb-2 mt-10">
             The Rise Of Something ...
           </h1>
           <h1 className="text-white text-sm font-bold">
@@ -86,8 +89,8 @@ if(loading){
         </div>
       </div>
       {(userRole==='Admin' &&
-         <div className="grid grid-cols-3 gap-5 h-30 mt-10">
-         <div className=" py-2 px-3 justify-between flex items-center">
+         <div className='grid grid-cols-3 gap-5 h-30 mt-10'>
+         <div className=" py-2 px-3 justify-between flex items-center col-span-1 border">
           <div>
           <h1 className="font-bold mb-3">Total active users</h1>
            <h1 className="font-bold text-3xl mb-3">18,765</h1>
@@ -98,7 +101,7 @@ if(loading){
           </div>
            <div className=""><Chart/></div>
          </div>
-         <div className="py-2 px-3 justify-between flex items-center">
+         <div className="py-2 px-3 justify-between flex items-center col-span-1 border">
           <div>
           <h1 className="font-bold mb-3">Total active users</h1>
            <h1 className="font-bold text-3xl mb-3">4,765</h1>
@@ -109,7 +112,7 @@ if(loading){
           </div>
          <div><Chart2/></div>
          </div>
-         <div className=" py-2 px-3 justify-between flex items-center">
+         <div className=" py-2 px-3 justify-between flex items-center col-span-1 border">
           <div>
           <h1 className="font-bold mb-3">Total active users</h1>
            <h1 className="font-bold text-3xl mb-3">678</h1>
@@ -123,13 +126,13 @@ if(loading){
        </div>
       )}
      
-     <div className="mt-15 grid grid-cols-3">
+     <div className="mt-15 grid grid-cols-3 gap-5">
       <div className="col-span-1">
         <h1 className="font-bold text-lg mb-2">Current download</h1>
         <p className="text-gray-500">Downloaded by operating system</p>
       <Piechart/>
       </div>
-<div className="col-span-2 ms-2">
+<div className="col-span-2">
   <div>
     <h1 className="font-bold text-lg mb-2">Area installed</h1>
     <p className="text-gray-500 mb-2">+43% than last year</p>
@@ -141,45 +144,45 @@ if(loading){
       <div className="mt-15 grid grid-cols-4">
         <div className="col-span-3 ">
           <h1 className="font-medium text-lg mb-5">New invoice</h1>
-          <table className="text-center w-200">
-            <thead className="text-gray-500">
+          <table className={`text-center w-200 border ${open ? 'w-[75%]':'w-[90%]'}`}>
+            <thead className="text-gray-500 border">
               <tr>
-                <th className="px-10 py-3">Invoice ID</th>
-                <th className="px-10 py-3">Category</th>
-                <th className="px-10 py-3">Price</th>
-                <th className="px-10 py-3">Status</th>
+                <th className="px-10 py-3 border">Invoice ID</th>
+                <th className="px-10 py-3 border">Category</th>
+                <th className="px-10 py-3 border">Price</th>
+                <th className="px-10 py-3 border">Status</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="py-5">INV-1990</td>
-                <td>Android</td>
-                <td>$83.74</td>
-                <td><p className="bg-green-200 text-green-700 font-bold w-10 ms-18 rounded-lg text-sm">Paid</p></td>
+              <tr className="border">
+                <td className="py-5 border">INV-1990</td>
+                <td className="border">Android</td>
+                <td className="border">$83.74</td>
+                <td className="border"><p className="bg-green-200 text-green-700 font-bold w-10 ms-18 rounded-lg text-sm">Paid</p></td>
               </tr>
-              <tr>
-                <td className="py-5">INV-1991</td>
-                <td>Mac</td>
-                <td>$97.14</td>
-                <td><p className="bg-red-200 text-red-700 font-bold w-25 ms-10 rounded-lg text-sm">Out of date</p></td>
+              <tr className="border">
+                <td className="py-5 border">INV-1991</td>
+                <td className="border">Mac</td>
+                <td className="border">$97.14</td>
+                <td className="border"><p className="bg-red-200 text-red-700 font-bold w-25 ms-10 rounded-lg text-sm">Out of date</p></td>
               </tr>
-              <tr>
-                <td className="py-5">INV-1992</td>
-                <td>Windows</td>
-                <td>$68.71</td>
-                <td><p className="bg-yellow-200 text-yellow-600 font-bold w-20 ms-13 rounded-lg text-sm">Progress</p></td>
+              <tr className="border">
+                <td className="py-5 border">INV-1992</td>
+                <td className="border">Windows</td>
+                <td className="border">$68.71</td>
+                <td className="border"><p className="bg-yellow-200 text-yellow-600 font-bold w-20 ms-13 rounded-lg text-sm">Progress</p></td>
               </tr>
-              <tr>
-                <td className="py-5">INV-1993</td>
-                <td>Android</td>
-                <td>$85.21</td>
-                <td><p className="bg-green-200 text-green-700 font-bold w-10 ms-18 rounded-lg text-sm">Paid</p></td>
+              <tr className="border">
+                <td className="py-5 border">INV-1993</td>
+                <td className="border">Android</td>
+                <td className="border">$85.21</td>
+                <td className="border"><p className="bg-green-200 text-green-700 font-bold w-10 ms-18 rounded-lg text-sm">Paid</p></td>
               </tr>
-              <tr>
-                <td className="py-5">INV-1994</td>
-                <td>Mac</td>
-                <td>$52.17</td>
-                <td><p className="bg-green-200 text-green-700 font-bold w-10 ms-18 rounded-lg text-sm">Paid</p></td>
+              <tr className="border">
+                <td className="py-5 border">INV-1994</td>
+                <td className="border">Mac</td>
+                <td className="border">$52.17</td>
+                <td className="border"><p className="bg-green-200 text-green-700 font-bold w-10 ms-18 rounded-lg text-sm">Paid</p></td>
               </tr>
             </tbody>
           </table>
@@ -196,11 +199,11 @@ if(loading){
                 <h1 className="font-bold text-gray-400">Free</h1>
               </div>
             </div>
-            <div className="flex text-gray-500 ms-13">
+            <div className="flex text-gray-500 ms-13 text-sm">
               <FaDownload className="mt-1 me-1" />
-              <p className="me-2">9.91k &#x2022;</p>
+              <p className="me-2">9.91k</p>
               <FaHardDrive className="mt-1 me-1" />
-              <p className="me-2">9.68 Mb &#x2022;</p>
+              <p className="me-2">9.68 Mb</p>
               <FaStar className="mt-1 me-1" />
               <p>9.91k</p>
             </div>
@@ -215,11 +218,11 @@ if(loading){
                 <h1 className="font-bold text-gray-400">Free</h1>
               </div>
             </div>
-            <div className="flex text-gray-500 ms-13">
+            <div className="flex text-gray-500 ms-13 text-sm">
               <FaDownload className="mt-1 me-1" />
-              <p className="me-2">1.95k &#x2022;</p>
+              <p className="me-2">1.95k</p>
               <FaHardDrive className="mt-1 me-1" />
-              <p className="me-2">1.9Mb &#x2022;</p>
+              <p className="me-2">1.9Mb</p>
               <FaStar className="mt-1 me-1" />
               <p>1.95k</p>
             </div>
@@ -234,11 +237,11 @@ if(loading){
                 <h1 className="font-bold text-gray-400">Free</h1>
               </div>
             </div>
-            <div className="flex text-gray-500 ms-13">
+            <div className="flex text-gray-500 ms-13 text-sm">
               <FaDownload className="mt-1 me-1" />
-              <p className="me-2">9.12k &#x2022;</p>
+              <p className="me-2">9.12k</p>
               <FaHardDrive className="mt-1 me-1" />
-              <p className="me-2">8.91 Mb &#x2022;</p>
+              <p className="me-2">8.91 Mb</p>
               <FaStar className="mt-1 me-1" />
               <p>9.12k</p>
             </div>
@@ -253,11 +256,11 @@ if(loading){
                 <h1 className="font-bold text-gray-400">Free</h1>
               </div>
             </div>
-            <div className="flex text-gray-500 ms-13">
+            <div className="flex text-gray-500 ms-13 text-sm">
               <FaDownload className="mt-1 me-1" />
-              <p className="me-2">6.98k &#x2022;</p>
+              <p className="me-2">6.98k</p>
               <FaHardDrive className="mt-1 me-1" />
-              <p className="me-2">6.82 Mb &#x2022;</p>
+              <p className="me-2">6.82</p>
               <FaStar className="mt-1 me-1" />
               <p>6.98k</p>
             </div>
@@ -272,11 +275,11 @@ if(loading){
                 <h1 className="font-bold text-gray-400">Free</h1>
               </div>
             </div>
-            <div className="flex text-gray-500 ms-13">
+            <div className="flex text-gray-500 ms-13 text-sm">
               <FaDownload className="mt-1 me-1" />
-              <p className="me-2">8.49k &#x2022;</p>
+              <p className="me-2">8.49k</p>
               <FaHardDrive className="mt-1 me-1" />
-              <p className="me-2">8.29 Mb &#x2022;</p>
+              <p className="me-2">8.29 Mb</p>
               <FaStar className="mt-1 me-1" />
               <p>8.49k</p>
             </div>
@@ -382,7 +385,7 @@ if(loading){
           </div>
         </div>
 
-        <div className=" col-span-1 justify-items-end">
+        <div className=" col-span-1">
           <div className="bg-green-800 h-30 w-85 mb-5 rounded-2xl flex px-3 py-2 items-center">
             <h1 className="text-xl text-white font-medium ms-5">48%</h1>
             <div className="ms-10">
